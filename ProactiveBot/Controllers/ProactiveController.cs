c52de5a10b17379d9ba8908a4b6834481e1babc7
Jeffery Cordes <jcordes@cardinalsolutions.com>
2019-01-11 12:17:00 -0500

    initial

---
+++ ProactiveBot/Controllers/ProactiveController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration;
+using Microsoft.Bot.Schema;
+using ProactiveBot.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProactiveBot.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProactiveController : Controller
+    {
+        private readonly BotFrameworkAdapter _adapter;
+
+        public ProactiveController(IAdapterIntegration adapter)
+        {
+            this._adapter = (BotFrameworkAdapter)adapter;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<ObjectResult> Post([FromBody]ProactiveMessage proMsg)
+        {
+            await _adapter.ContinueConversationAsync(proMsg.BotId, proMsg.Conversation, async (context, token) =>
+            {
+                await context.SendActivityAsync(proMsg.MessagePayload);
+            }, new System.Threading.CancellationToken());
+
+            return Ok("Message Sent");
+        }
+
+    }
+}
---
+++ ProactiveBot/Models/ProactiveMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Schema;
+
+namespace ProactiveBot.Models
+{
+    public class ProactiveMessage
+    {
+        public string BotId { get; set; }
+        public ConversationReference Conversation { get; set; }
+        public string MessagePayload { get; set; }
+    }
+}
